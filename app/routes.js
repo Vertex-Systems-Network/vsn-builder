ee2588902ac3560546b4c0d70229f3b45cf2d529
WPEssential <wordpress.essential@gmail.com>
2026-09-17 14:29:58 +0500

    fix: exclude proxy modules from filesystem routing

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,6 @@ import { flatRoutes } from "@react-router/fs-routes";
 export default [
   route("builder-proxy/*", "./routes/builder-proxy-secure.$.jsx"),
   ...(await flatRoutes({
-    ignoredRouteFiles: ["builder-proxy.$.jsx", "builder-proxy-secure.$.jsx"],
+    ignoredRouteFiles: ["**/builder-proxy.$.jsx", "**/builder-proxy-secure.$.jsx"],
   })),
 ];
