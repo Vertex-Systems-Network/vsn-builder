665186b94e140654d917ca66ce8125b25dc5c93d
WPEssential <wordpress.essential@gmail.com>
2026-09-17 14:21:56 +0500

    security: bind builder proxy to hardened mutation route

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,9 @@
+import { route } from "@react-router/dev/routes";
 import { flatRoutes } from "@react-router/fs-routes";
 
-export default flatRoutes();
+export default [
+  route("builder-proxy/*", "./routes/builder-proxy-secure.$.jsx"),
+  ...(await flatRoutes({
+    ignoredRouteFiles: ["builder-proxy.$.jsx", "builder-proxy-secure.$.jsx"],
+  })),
+];
